5434af8e848eb912e4a009e7f812e38b4dec2870
vinayaknandan2 <vinayaknandan2@gmail.com>
2025-11-09 13:40:42 +0000

    container config done

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId=124445
+let emailid="abcd@gmail.com"
+var name ="Vinayak"
+cityname="Jaipur"
+emailid="k@gmail.com"
+name="Nandan"
+cityname="Mumbai"
+
+console.table([accountId,emailid,name,cityname])
