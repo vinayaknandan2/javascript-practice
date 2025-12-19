db30f5bc72b989b8b80fc7a15dd638721738392a
vinayaknandan2 <vinayaknandan2@gmail.com>
2025-12-19 06:17:28 +0000

    container config done

---
+++ basics/practice.js
@@ -0,0 +1,20 @@
+// console.log("Vinayak")
+// console.log("Aekansh")
+// for(let i=0;i<5;i++){
+//     console.log("This is my first javascript program")
+// }
+// console.log(typeof undefined)
+// console.log(typeof null)
+// console.log(typeof 1)
+// console.log(typeof "Vinayak")
+// console.log(typeof {})
+// let decision=false
+// console.log(decision)
+// console.log(typeof decision)
+let score = "Vinayak"
+console.log(score)
+console.log(typeof score)
+console.log(typeof (score))
+let valueInnumber=Number(score)
+console.log(typeof valueInnumber)
+console.log(typeof score)
